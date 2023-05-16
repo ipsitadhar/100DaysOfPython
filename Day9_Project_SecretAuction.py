04ec3be9eb7ea3979aee37df06d86eb4f58f1658
ipsitdhar <ipsitadhar@gmail.com>
2023-05-16 23:55:43 +0530

    Day 9 programs

---
+++ Day9_Project_SecretAuction.py
@@ -0,0 +1,22 @@
+import platform
+import os
+#HINT: You can call clear() to clear the output in the console.
+from art1 import logo
+print(logo)
+
+more_bids=True
+auction_data={}
+
+while more_bids:
+  name=input("What is your name?")
+  bid=int(input("what's your bid? $"))
+  auction_data[name]=bid
+  option=input("Are there any other bidders? Type 'yes' or 'no'")
+  if option=='no':
+    print(auction_data)
+    highest_bidder=max(auction_data)
+    highest_bid=max(auction_data.values())
+    print(f"The winner is {highest_bidder} with a bid of ${highest_bid}")
+    more_bids=False
+    
+    

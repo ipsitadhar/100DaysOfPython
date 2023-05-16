d8f016e51110e150b93d9c934f02e151620f3d08
ipsitdhar <ipsitadhar@gmail.com>
2023-05-16 23:55:43 +0530

    Day 9 programs

---
+++ Day9_NestingListsAndDictionaries.py
@@ -0,0 +1,36 @@
+#nesting
+capital={
+    "Germany":"Berlin",
+    "France":"Paris",
+
+}
+
+#Nesting a list in the dictionary
+travel_log={
+    "France":["Paris","Nice"],
+    "Germany":["Berlin","Stuttgart"],
+              
+}
+
+#Nesting a dictionary in a dictionary
+travel_log={
+    "France":{"cities_visited":["Paris","Nice"],"Cities_pending":"Dijon"},
+    "Germany":["Berlin","Stuttgart"],
+              
+}
+
+#Nesting a dictionary inside a list
+travel_log=[
+    {
+        "country":"France",
+        "cities_visited":["Paris","Nice"],
+        "Cities_pending":"Dijon",
+        "visits":1,
+    },
+    {
+        "country":"Germany",
+        "cities_visited":["Berlin","Stuttgart"],
+        "Cities_pending":"Frankfurt",
+        "visits":2,
+    },
+]

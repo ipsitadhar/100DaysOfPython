cf43d24d46a8f81d56b6a6481b0d8535ec754d8f
ipzi <ipsitadhar@gmail.com>
2023-06-04 16:59:45 +0530

    Day 17 files

--- Day17/quiz-game-start/data.py
+++ Day17/quiz-game-start/data.py
@@ -1,18 +1,37 @@
-question_data = [
-    {"text": "A slug's blood is green.", "answer": "True"},
-    {"text": "The loudest animal is the African Elephant.", "answer": "False"},
-    {"text": "Approximately one quarter of human bones are in the feet.", "answer": "True"},
-    {"text": "The total surface area of a human lungs is the size of a football pitch.", "answer": "True"},
-    {
-        "text": "In West Virginia, USA, if you accidentally hit an animal with your car, "
-                "you are free to take it home to eat.",
-        "answer": "True"},
-    {"text": "In London, UK, if you happen to die in the House of Parliament, you are entitled to a state funeral.",
-     "answer": "False"},
-    {"text": "It is illegal to pee in the Ocean in Portugal.", "answer": "True"},
-    {"text": "You can lead a cow down stairs but not up stairs.", "answer": "False"},
-    {"text": "Google was originally called 'Backrub'.", "answer": "True"},
-    {"text": "Buzz Aldrin's mother's maiden name was 'Moon'.", "answer": "True"},
-    {"text": "No piece of square dry paper can be folded in half more than 7 times.", "answer": "False"},
-    {"text": "A few ounces of chocolate can to kill a small dog.", "answer": "True"}
-]
+question_data =[
+    {"category": "General Knowledge",
+     "type": "boolean",
+     "difficulty": "easy",
+     "question": "The Great Wall of China is visible from the moon.",
+     "correct_answer": "False",
+     "incorrect_answers": ["True"]},
+
+    {"category": "General Knowledge", "type": "boolean", "difficulty": "easy",
+     "question": "French is an official language in Canada.", "correct_answer": "True",
+     "incorrect_answers": ["False"]},
+    {"category": "General Knowledge", "type": "boolean", "difficulty": "easy",
+     "question": "Nutella is produced by the German company Ferrero.", "correct_answer": "False",
+     "incorrect_answers": ["True"]},
+    {"category": "General Knowledge", "type": "boolean", "difficulty": "easy",
+     "question": "&quot;Ananas&quot; is mostly used as the word for Pineapple in other languages.",
+     "correct_answer": "True", "incorrect_answers": ["False"]},
+    {"category": "General Knowledge", "type": "boolean", "difficulty": "easy",
+     "question": "In 2010, Twitter and the United States Library of Congress partnered "
+                 "together to archive every tweet by American citizens.",
+     "correct_answer": "True", "incorrect_answers": ["False"]},
+    {"category": "General Knowledge", "type": "boolean", "difficulty": "easy", "question": "Pluto is a planet.",
+     "correct_answer": "False", "incorrect_answers": ["True"]},
+    {"category": "General Knowledge", "type": "boolean", "difficulty": "easy",
+     "question": "Romanian belongs to the Romance language family, shared"
+                 " with French, Spanish, Portuguese and Italian. ",
+     "correct_answer": "True", "incorrect_answers": ["False"]},
+    {"category": "General Knowledge", "type": "boolean", "difficulty": "easy",
+     "question": "Dihydrogen Monoxide was banned due to health risks after being "
+                 "discovered in 1983 inside swimming pools and drinking water.",
+     "correct_answer": "False", "incorrect_answers": ["True"]},
+    {"category": "General Knowledge", "type": "boolean", "difficulty": "easy",
+     "question": "Vietnam&#039;s national flag is a red star in front of a yellow background.",
+     "correct_answer": "False", "incorrect_answers": ["True"]},
+    {"category": "General Knowledge", "type": "boolean", "difficulty": "easy",
+     "question": "Jingle Bells was originally meant for Thanksgiving", "correct_answer": "True",
+     "incorrect_answers": ["False"]}]
